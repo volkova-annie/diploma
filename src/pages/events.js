5adcf52b9eedcd2aaec94d5f31fe74ff483130e9
Anna Volkova <annie_v@bk.ru>
2017-08-20 18:44:05 +0300

    add events page

---
+++ src/pages/events.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const EventsPage = () =>
+  <div>
+    <h1>Events</h1>
+    <Link to="/">Go to index</Link>
+  </div>
+
+export default EventsPage
