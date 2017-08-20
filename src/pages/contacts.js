4423207733cdd8ef0f60ff31b8a203e2b927591b
Anna Volkova <annie_v@bk.ru>
2017-08-20 18:43:46 +0300

    add contacts page

---
+++ src/pages/contacts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ContactsPage = () =>
+  <div>
+    <h1>Contacts</h1>
+    <Link to="/">Go to index</Link>
+  </div>
+
+export default ContactsPage
