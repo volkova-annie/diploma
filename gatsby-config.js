2245721dbb30792dbaa7ab20f1674d1599f69b9b
Alexander Kurganov <me@akurganow.ru>
2017-08-20 18:24:31 +0300

    Add contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Cuba Bar - Golohovaya 28`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'pp17w473qhjl',
+        accessToken: 'f48298775421dff693fb6baf54cb7683632fbdf6bbac0f9704ea4a3c8e405db3',
+      },
+    },
+  ],
 }
