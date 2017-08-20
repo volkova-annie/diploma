00fb4549168f211e1235768a89019ddd1b1905de
Anna Volkova <annie_v@bk.ru>
2017-08-20 18:44:15 +0300

    add gallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const GalleryPage = () =>
+  <div>
+    <h1>Gallery</h1>
+    <Link to="/">Go to index</Link>
+  </div>
+
+export default GalleryPage
