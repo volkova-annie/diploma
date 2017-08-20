ad82b8b9cc67de15b96dcafb3dcf733ede17bb6e
Anna Volkova <annie_v@bk.ru>
2017-08-20 18:47:57 +0300

    add menu page

---
+++ src/pages/menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const MenuPage = () =>
+  <div>
+    <h1>Menu</h1>
+    <Link to="/">Go to index</Link>
+  </div>
+
+export default MenuPage
