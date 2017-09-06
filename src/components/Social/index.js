7bfc008da4f8e2c7bbeeb1964014e46dc557288b
Anna Volkova <annie_v@bk.ru>
2017-09-06 22:24:19 +0300

    modify Social component

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -5,7 +5,7 @@ import {Sprite, Icon} from '../SVGIcon'
 
 const Social = () =>
   <ul className={st.social}>
-    <li className={st.item}><a href='tel:+79211234567'><Icon className={st.icon} icon='call' /></a></li>
+    {/* <li className={st.item}><a href='tel:+79211234567'><Icon className={st.icon} icon='call' /></a></li> */}
     <li className={st.item}><a href='vk.com'><Icon className={st.icon} icon='vk' /></a></li>
     <li className={st.item}><a href='instagram.com'><Icon className={st.icon} icon='instagram' /></a></li>
   </ul>
