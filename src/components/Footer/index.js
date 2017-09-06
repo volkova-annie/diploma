200799b209fff824cec162663629d7e03ad96613
Anna Volkova <annie_v@bk.ru>
2017-09-06 23:08:47 +0300

    add link to address

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ import st from './style.module.css'
 const Footer = () =>
   <footer className={st.footer}>
     <div className={st.top}>
-      <Iconed icon='place' title='АДРЕС' subtitle='Санкт-Петербург, Гороховая, 28'/>
+      <Iconed link='https://maps.google.com?q=Cuba+Bar,+Gorohovaya+28' icon='place' title='АДРЕС' subtitle='Санкт-Петербург, Гороховая, 28'/>
       <Iconed link='tel:+79754565585' icon='call' title='КОНТАКТНЫЙ ТЕЛЕФОН' subtitle='+7 (123) 456-78-90'/>
       <Iconed icon='clock' title='ВРЕМЯ РАБОТЫ' subtitle='15:00 - 03:00'/>
       <Iconed icon='dinner' title='ЗАБРОНИРОВАТЬ СТОЛИК' subtitle='Онлайн бронирование'/>
--- src/components/Iconed/index.js
+++ src/components/Iconed/index.js
@@ -5,7 +5,7 @@ import {Sprite, Icon} from '../SVGIcon'
 
 const Iconed = (props) => {
   if (props.link) {
-    return <a className={st.iconed} href={props.link}>
+    return <a className={st.iconed} href={props.link} target='_blank'>
       <Icon className={st.icon} icon={props.icon} />
       <p className={st.iconed_title}>{props.title}</p>
       <p className={st.iconed_text}>{props.subtitle}</p>
