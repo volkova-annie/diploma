3c24af43df7a661602014715d5cf778d2ce19ac8
Anna Volkova <annie_v@bk.ru>
2017-09-06 22:24:08 +0300

    modify Navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import st from './style.module.css'
+import Locale from '../Locale'
 
 const Navbar = () =>
   <ul className={st.navbar}>
@@ -8,6 +9,7 @@ const Navbar = () =>
     <li className={st.item}><Link className={st.link} to='/gallery'>Галерея</Link></li>
     <li className={st.item}><Link className={st.link} to='/menu'>Меню</Link></li>
     <li className={st.item}><Link className={st.link} to='/contacts'>Контакты</Link></li>
+    <li className={st.item}><Locale></Locale></li>
   </ul>
 
 export default Navbar
