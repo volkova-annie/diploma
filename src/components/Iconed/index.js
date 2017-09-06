cabaff8a3c7f8312d03d1bd3ee640ea3f0d19cd5
Anna Volkova <annie_v@bk.ru>
2017-09-06 22:25:04 +0300

    add Iconed component

---
+++ src/components/Iconed/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import st from './style.module.css'
+import {Sprite, Icon} from '../SVGIcon'
+
+const Iconed = (props) => {
+  if (props.link) {
+    return <a className={st.iconed} href={props.link}>
+      <Icon className={st.icon} icon={props.icon} />
+      <p className={st.iconed_title}>{props.title}</p>
+      <p className={st.iconed_text}>{props.subtitle}</p>
+    </a>
+  }
+  else {
+    return <span className={st.iconed}>
+      <Icon className={st.icon} icon={props.icon} />
+      <p className={st.iconed_title}>{props.title}</p>
+      <p className={st.iconed_text}>{props.subtitle}</p>
+    </span>
+  }
+}
+
+export default Iconed
