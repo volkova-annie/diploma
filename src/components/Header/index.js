2fc5f4fd8584881dfb68d0ed7bd82f5b96b721fd
Anna Volkova <annie_v@bk.ru>
2017-09-13 23:09:57 +0300

    add Phone number add logo to Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,16 @@ import Navbar from '../Navbar'
 import Social from '../Social'
 import Locale from '../Locale'
 import Link from 'gatsby-link'
+import Iconed from '../Iconed'
+import Phone from '../Phone'
 import st from './style.module.css'
 
 const Header = () =>
   <header className={st.header}>
     <div className={st.top}>
+      <Phone/>
+      <Iconed className={st.logo} icon='cuba' title='Cuba-bar' subtitle=''/>
       <Social></Social>
-      {/* <Locale></Locale> */}
     </div>
     <Navbar></Navbar>
   </header>
