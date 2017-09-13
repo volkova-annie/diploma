8301aa1a62bf1e17db10221f06a8cfac4281a520
Anna Volkova <annie_v@bk.ru>
2017-09-13 23:10:40 +0300

    modify index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Slider from '../components/Slider'
 
 const IndexPage = () =>
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Slider></Slider>
   </div>
 
 export default IndexPage
