9c8ff5cf5ff2551543daf843a342fedb8040e94e
Alexander Kurganov <me@akurganow.ru>
2017-09-29 21:12:53 +0300

    Update Iconed

--- src/components/Iconed/index.js
+++ src/components/Iconed/index.js
@@ -5,15 +5,24 @@ import {Sprite, Icon} from '../SVGIcon'
 
 const Iconed = (props) => {
   if (props.link) {
-    return <a className={st.iconed} href={props.link} target='_blank'>
-      <Icon className={st.icon} icon={props.icon} />
+    return <a
+      className={st.iconed}
+      href={props.link}
+      target='_blank'>
+      <Icon
+        className={st.icon}
+        icon={props.icon} />
       <p className={st.iconed_title}>{props.title}</p>
       <p className={st.iconed_text}>{props.subtitle}</p>
     </a>
   }
   else {
-    return <span className={st.iconed}>
-      <Icon className={st.icon} icon={props.icon} />
+    return <span
+      onClick={props.onClick}
+      className={st.iconed}>
+      <Icon
+        className={st.icon}
+        icon={props.icon} />
       <p className={st.iconed_title}>{props.title}</p>
       <p className={st.iconed_text}>{props.subtitle}</p>
     </span>
