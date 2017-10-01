749b515f8a0c1cf83c78edccacedc02667131dac
Anna Volkova <annie_v@bk.ru>
2017-10-01 19:49:08 +0300

    modify component Phone

--- src/components/Phone/index.js
+++ src/components/Phone/index.js
@@ -5,8 +5,14 @@ import Link from 'gatsby-link'
 import st from './style.module.css'
 
 const Phone = () =>
-  <a className={st.phone} href='tel:+79211233456'>
-    <Iconed className={st.call} icon='call' title='' subtitle=''/>
+  <a
+    className={st.phone}
+    href='tel:+79211233456'>
+    <Iconed
+      className={st.call}
+      icon='call'
+      title=''
+      subtitle='' />
     <span className={st.number}>+7 (921) 123-34-56</span>
   </a>
 
