f3e83a023ca9490d799d260d2eb0da4c43d98d57
Anna Volkova <annie_v@bk.ru>
2017-10-01 19:47:50 +0300

    modify component Actions

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -5,29 +5,26 @@ import Link from 'gatsby-link'
 import st from './style.module.css'
 
 const Actions = () =>
-  <actions className={st.actions}>
-    <div className={st.top}>
-      <Iconed
-        link='https://maps.google.com?q=Cuba+Bar,+Gorohovaya+28'
-        icon='place'
-        title='АДРЕС'
-        subtitle='Санкт-Петербург, Гороховая, 28' />
-      <Iconed
-        link='tel:+79754565585'
-        icon='call'
-        title='КОНТАКТНЫЙ ТЕЛЕФОН'
-        subtitle='+7 (123) 456-78-90' />
-      <Iconed
-        icon='clock'
-        title='ВРЕМЯ РАБОТЫ'
-        subtitle='15:00 - 03:00' />
-      <Iconed
-        onClick={window.openModal.bind(null, 'Booking')}
-        icon='dinner'
-        title='ЗАБРОНИРОВАТЬ СТОЛИК'
-        subtitle='' />
-    </div>
-
-  </actions>
+  <div className={st.actions}>
+    <Iconed
+      link='https://maps.google.com?q=Cuba+Bar,+Gorohovaya+28'
+      icon='place'
+      title='АДРЕС'
+      subtitle='Санкт-Петербург, Гороховая, 28' />
+    <Iconed
+      link='tel:+79754565585'
+      icon='call'
+      title='КОНТАКТНЫЙ ТЕЛЕФОН'
+      subtitle='+7 (123) 456-78-90' />
+    <Iconed
+      icon='clock'
+      title='ВРЕМЯ РАБОТЫ'
+      subtitle='15:00 - 03:00' />
+    <Iconed
+      onClick={window.openModal.bind(null, 'Booking')}
+      icon='dinner'
+      title='ЗАБРОНИРОВАТЬ СТОЛИК'
+      subtitle='' />
+  </div>
 
 export default Actions
