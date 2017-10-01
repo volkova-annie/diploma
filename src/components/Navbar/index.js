f42bb3d25ef54f4b521ad02e2a7319160083d9f0
Anna Volkova <annie_v@bk.ru>
2017-10-01 19:48:39 +0300

    modify component Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,11 +5,34 @@ import Locale from '../Locale'
 
 const Navbar = () =>
   <ul className={st.navbar}>
-    <li className={st.item}><Link className={st.link} to='/events'>События</Link></li>
-    <li className={st.item}><Link className={st.link} to='/gallery'>Галерея</Link></li>
-    <li className={st.item}><Link className={st.link} to='/menu'>Меню</Link></li>
-    <li className={st.item}><Link className={st.link} to='/contacts'>Контакты</Link></li>
-    <li className={st.item}><Locale></Locale></li>
+    <li className={st.item}>
+      <Link
+        className={st.link}
+        to='/events'>События
+      </Link>
+    </li>
+    <li className={st.item}>
+      <Link
+        className={st.link}
+        to='/gallery'>Галерея
+      </Link>
+    </li>
+    <li className={st.item}>
+      <Link
+        className={st.link}
+        to='/menu'>Меню
+      </Link>
+    </li>
+    <li className={st.item}>
+      <Link
+        className={st.link}
+        to='/contacts'>Контакты
+      </Link>
+    </li>
+    <li className={st.item}>
+      <Locale>
+      </Locale>
+    </li>
   </ul>
 
 export default Navbar
