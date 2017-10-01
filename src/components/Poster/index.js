3a0f61e6e7294f52fc0251e1139b370b9693f08a
Anna Volkova <annie_v@bk.ru>
2017-10-01 19:49:25 +0300

    modify component Poster

--- src/components/Poster/index.js
+++ src/components/Poster/index.js
@@ -2,13 +2,24 @@ import React from 'react'
 import Link from 'gatsby-link'
 import st from './style.module.css'
 
-const Poster = (props) =>
-  <div className={st.poster}>
-    <img
-      className={st.img}
-      {...props.image.responsiveResolution} />
-    <p className={st.time}>{props.date} в {props.time}</p>
-    <p className={st.title}>{props.title}</p>
-  </div>
+const Poster = (props) => {
+  if (props.date == 'Invalid Date') {
+    return<div className={st.poster}>
+      <img
+        className={st.img}
+        {...props.image.responsiveResolution} />
+      <p className={st.title}>{props.title}</p>
+    </div>
+  }
+  else {
+    return<div className={st.poster}>
+      <img
+        className={st.img}
+        {...props.image.responsiveResolution} />
+      <p className={st.time}>{props.date} в {props.time}</p>
+      <p className={st.title}>{props.title}</p>
+    </div>
+  }
+}
 
 export default Poster
