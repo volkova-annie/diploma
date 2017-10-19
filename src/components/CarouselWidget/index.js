b42f5419869ef8003b3e7bf4495fd0c020017ced
Alexander Kurganow <me@akurganow.ru>
2017-10-19 16:58:37 +0300

    fix colors

--- src/components/CarouselWidget/index.js
+++ src/components/CarouselWidget/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 import st from './style.module.css'
 import img1 from '../../../assets/carousel/1.jpg'
 import img2 from '../../../assets/carousel/2.jpg'
 import img3 from '../../../assets/carousel/3.jpg'
 import img4 from '../../../assets/carousel/4.jpg'
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-const CarouselWidget = props =>
+const CarouselWidget = props => (
   // <Carousel showArrows='true' onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
   <Carousel
+    className={st.carousel}
     showThumbs={false}
+    showStatus={false}
     infiniteLoop
     showArrows
     autoPlay
@@ -22,9 +24,10 @@ const CarouselWidget = props =>
         <img
           className={st.image}
           {...pic.node.picture.responsiveResolution} />
-        <p className={st.legend}>{pic.node.title}</p>
+        {/*<p className={st.legend}>{pic.node.title}</p>*/}
       </div>
     ))}
   </Carousel>
+)
 
 export default CarouselWidget
--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -8,14 +8,18 @@ const Social = props => (
   <ul className={cn(props.className, st.social)}>
     {/* <li className={st.item}><a href='tel:+79211234567'><Icon className={st.icon} icon='call' /></a></li> */}
     <li className={st.item}>
-      <a href="vk.com">
+      <a
+        className={st.link}
+        href="//vk.com">
         <Icon
           className={st.icon}
           icon="vk" />
       </a>
     </li>
     <li className={st.item}>
-      <a href="instagram.com">
+      <a
+        className={st.link}
+        href="//instagram.com">
         <Icon
           className={st.icon}
           icon="instagram" />
