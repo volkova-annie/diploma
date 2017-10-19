ce00562feb66d33e3bc67d91aa65ffc67f553c62
Alexander Kurganow <me@akurganow.ru>
2017-10-19 09:58:37 +0300

    Fix layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,30 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
-import {Sprite, Icon} from '../components/SVGIcon'
+import { Sprite, Icon } from '../components/SVGIcon'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import {Modals} from '../components/Modals'
+import { Modals } from '../components/Modals'
 
 import './style.css'
 
-const TemplateWrapper = ({ children }) =>
+const TemplateWrapper = ({ children }) => (
   <div>
     <Modals />
-    <Header></Header>
-    <div>
-      {children()}
-    </div>
+    <Header />
+    <div>{children()}</div>
 
-    <Footer></Footer>
+    <Footer />
     <Sprite />
-    <Helmet
-      title="Cuba"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]} />
+    <Helmet>
+      <title>Cuba Bar</title>
+      <meta
+        name="description"
+        content="Cuba Bar" />
+      <meta
+        name="keywords"
+        content="cuba, bar, gorohovoya" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i|Podkova:400,600&amp;subset=cyrillic,cyrillic-ext,latin-ext"
+        rel="stylesheet" />
+    </Helmet>
   </div>
+)
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
