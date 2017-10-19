85cc0daaabf9e96bfdb1815a9e7d8c02df996c04
Alexander Kurganow <me@akurganow.ru>
2017-10-19 11:17:50 +0300

    move about to page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: 'Cuba Bar - Golohovaya 28',
-    about: {
-      ru: `Russian Lorem ipsum dolor sit amet, veniam expetenda intellegebat quo et, mel ei nostro volutpat facilisis. Pro eros solet ornatus ea. Mel graecis commune maluisset cu, illum iuvaret principes eam in, an qui nisl justo neglegentur. Cum at mentitum accommodare.
-Et tota falli epicurei per, ea tibique ponderum eam. Usu te causae impedit, idque vitae vis ea. Ne mutat putant habemus cum. Duo bonorum laoreet ex. Veri facilisis accusamus vis ex, ad est cetero audiam democritum. Ea vel admodum accusamus, an vim virtute repudiandae, dico dignissim sit te. Mei putent probatus cu.
-Simul altera nostrud mel et, ex delenit laoreet quo, mundi facilis vivendum vim ex. Qui id commune consulatu urbanitas. Usu cu vocent impetus aliquando, nonumy petentium consequuntur eu eam. Et his primis sensibus torquatos, audire epicuri id per. Ei ius civibus accusata constituam, est et vivendo consetetur.
-Id ius atqui populo, cu saperet suscipit copiosae mei. Vis no debet erant moderatius, augue inermis docendi at mei. Mel autem idque menandri et. Duo dictas alterum at.
-Idque molestiae his ut, an mel corrumpit voluptatum. No pri hendrerit inciderint, eu mazim tempor efficiendi ius. Eu case ferri autem mel, mea lucilius incorrupte neglegentur ut. Ne mel nibh semper latine, vero doming eam ad, iudico melius eu sea. Nam graece dolorem mandamus ea.
-Usu te evertitur vulputate. Eu quaeque efficiantur eos. Cu dolorem iracundia gloriatur quo. Ad augue virtute vel, ius cu dicam animal, cum tota aeque ne.
-Duo ei sale nonumes. Qui ad congue admodum. Utroque laoreet similique ei per, altera scripta admodum qui no, ut aliquam molestie ius. Dolore ubique ei mei. Legere viderer vim et. Eum id voluptatum delicatissimi.
-Ne choro nominati vim. An vix omnes feugiat senserit, eos amet patrioque gubergren ei, laboramus conclusionemque nam et. Nec ad veri doming admodum, suas augue movet vix at, in brute reprehendunt mea. Duo ad brute dicit reprimique, malis consetetur an ius.`,
-      en: `English Lorem ipsum dolor sit amet, veniam expetenda intellegebat quo et, mel ei nostro volutpat facilisis. Pro eros solet ornatus ea. Mel graecis commune maluisset cu, illum iuvaret principes eam in, an qui nisl justo neglegentur. Cum at mentitum accommodare.
-Et tota falli epicurei per, ea tibique ponderum eam. Usu te causae impedit, idque vitae vis ea. Ne mutat putant habemus cum. Duo bonorum laoreet ex. Veri facilisis accusamus vis ex, ad est cetero audiam democritum. Ea vel admodum accusamus, an vim virtute repudiandae, dico dignissim sit te. Mei putent probatus cu.
-Simul altera nostrud mel et, ex delenit laoreet quo, mundi facilis vivendum vim ex. Qui id commune consulatu urbanitas. Usu cu vocent impetus aliquando, nonumy petentium consequuntur eu eam. Et his primis sensibus torquatos, audire epicuri id per. Ei ius civibus accusata constituam, est et vivendo consetetur.
-Id ius atqui populo, cu saperet suscipit copiosae mei. Vis no debet erant moderatius, augue inermis docendi at mei. Mel autem idque menandri et. Duo dictas alterum at.
-Idque molestiae his ut, an mel corrumpit voluptatum. No pri hendrerit inciderint, eu mazim tempor efficiendi ius. Eu case ferri autem mel, mea lucilius incorrupte neglegentur ut. Ne mel nibh semper latine, vero doming eam ad, iudico melius eu sea. Nam graece dolorem mandamus ea.
-Usu te evertitur vulputate. Eu quaeque efficiantur eos. Cu dolorem iracundia gloriatur quo. Ad augue virtute vel, ius cu dicam animal, cum tota aeque ne.
-Duo ei sale nonumes. Qui ad congue admodum. Utroque laoreet similique ei per, altera scripta admodum qui no, ut aliquam molestie ius. Dolore ubique ei mei. Legere viderer vim et. Eum id voluptatum delicatissimi.
-Ne choro nominati vim. An vix omnes feugiat senserit, eos amet patrioque gubergren ei, laboramus conclusionemque nam et. Nec ad veri doming admodum, suas augue movet vix at, in brute reprehendunt mea. Duo ad brute dicit reprimique, malis consetetur an ius.`,
-    },
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,18 +13,42 @@ const IndexPage = props => {
   const localesData = transformLocales(props.data, props.locales)
   const currentLocale = props.locale
   const { events, menu, gallery, slider } = localesData[currentLocale]
+  const { t } = props
 
   return (
     <div>
       <CarouselWidget pictures={slider.edges} />
       <Actions />
-      <h2 className={st.header}>О Кубе</h2>
-      <p className={st.text} />
-      <h2 className={st.header}>События</h2>
-      <p className={st.subtitle}>Приходите к нам у нас весело и вкусно!</p>
+      <h2 className={st.header}>{t({ ru: 'О Кубе', en: 'About Cuba' })}</h2>
+      <p className={st.text}>
+        {t({
+          ru: `Russian Lorem ipsum dolor sit amet, veniam expetenda intellegebat quo et, mel ei nostro volutpat facilisis. Pro eros solet ornatus ea. Mel graecis commune maluisset cu, illum iuvaret principes eam in, an qui nisl justo neglegentur. Cum at mentitum accommodare.
+Et tota falli epicurei per, ea tibique ponderum eam. Usu te causae impedit, idque vitae vis ea. Ne mutat putant habemus cum. Duo bonorum laoreet ex. Veri facilisis accusamus vis ex, ad est cetero audiam democritum. Ea vel admodum accusamus, an vim virtute repudiandae, dico dignissim sit te. Mei putent probatus cu.
+Simul altera nostrud mel et, ex delenit laoreet quo, mundi facilis vivendum vim ex. Qui id commune consulatu urbanitas. Usu cu vocent impetus aliquando, nonumy petentium consequuntur eu eam. Et his primis sensibus torquatos, audire epicuri id per. Ei ius civibus accusata constituam, est et vivendo consetetur.
+Id ius atqui populo, cu saperet suscipit copiosae mei. Vis no debet erant moderatius, augue inermis docendi at mei. Mel autem idque menandri et. Duo dictas alterum at.
+Idque molestiae his ut, an mel corrumpit voluptatum. No pri hendrerit inciderint, eu mazim tempor efficiendi ius. Eu case ferri autem mel, mea lucilius incorrupte neglegentur ut. Ne mel nibh semper latine, vero doming eam ad, iudico melius eu sea. Nam graece dolorem mandamus ea.
+Usu te evertitur vulputate. Eu quaeque efficiantur eos. Cu dolorem iracundia gloriatur quo. Ad augue virtute vel, ius cu dicam animal, cum tota aeque ne.
+Duo ei sale nonumes. Qui ad congue admodum. Utroque laoreet similique ei per, altera scripta admodum qui no, ut aliquam molestie ius. Dolore ubique ei mei. Legere viderer vim et. Eum id voluptatum delicatissimi.
+Ne choro nominati vim. An vix omnes feugiat senserit, eos amet patrioque gubergren ei, laboramus conclusionemque nam et. Nec ad veri doming admodum, suas augue movet vix at, in brute reprehendunt mea. Duo ad brute dicit reprimique, malis consetetur an ius.`,
+          en: `English Lorem ipsum dolor sit amet, veniam expetenda intellegebat quo et, mel ei nostro volutpat facilisis. Pro eros solet ornatus ea. Mel graecis commune maluisset cu, illum iuvaret principes eam in, an qui nisl justo neglegentur. Cum at mentitum accommodare.
+Et tota falli epicurei per, ea tibique ponderum eam. Usu te causae impedit, idque vitae vis ea. Ne mutat putant habemus cum. Duo bonorum laoreet ex. Veri facilisis accusamus vis ex, ad est cetero audiam democritum. Ea vel admodum accusamus, an vim virtute repudiandae, dico dignissim sit te. Mei putent probatus cu.
+Simul altera nostrud mel et, ex delenit laoreet quo, mundi facilis vivendum vim ex. Qui id commune consulatu urbanitas. Usu cu vocent impetus aliquando, nonumy petentium consequuntur eu eam. Et his primis sensibus torquatos, audire epicuri id per. Ei ius civibus accusata constituam, est et vivendo consetetur.
+Id ius atqui populo, cu saperet suscipit copiosae mei. Vis no debet erant moderatius, augue inermis docendi at mei. Mel autem idque menandri et. Duo dictas alterum at.
+Idque molestiae his ut, an mel corrumpit voluptatum. No pri hendrerit inciderint, eu mazim tempor efficiendi ius. Eu case ferri autem mel, mea lucilius incorrupte neglegentur ut. Ne mel nibh semper latine, vero doming eam ad, iudico melius eu sea. Nam graece dolorem mandamus ea.
+Usu te evertitur vulputate. Eu quaeque efficiantur eos. Cu dolorem iracundia gloriatur quo. Ad augue virtute vel, ius cu dicam animal, cum tota aeque ne.
+Duo ei sale nonumes. Qui ad congue admodum. Utroque laoreet similique ei per, altera scripta admodum qui no, ut aliquam molestie ius. Dolore ubique ei mei. Legere viderer vim et. Eum id voluptatum delicatissimi.
+Ne choro nominati vim. An vix omnes feugiat senserit, eos amet patrioque gubergren ei, laboramus conclusionemque nam et. Nec ad veri doming admodum, suas augue movet vix at, in brute reprehendunt mea. Duo ad brute dicit reprimique, malis consetetur an ius.`,
+        })}
+      </p>
+      <h2 className={st.header}>{t({ ru: 'События', en: 'Events' })}</h2>
+      <p className={st.subtitle}>
+        {t({ ru: 'Приходите к нам у нас весело и вкусно!', en: 'Come to us we have fun and delicious!' })}
+      </p>
       <Collage items={events.edges} />
-      <h2 className={st.header}>Меню</h2>
-      <p className={st.subtitle}>Приходите к нам у нас весело и вкусно!</p>
+      <h2 className={st.header}>{t({ ru: 'Меню', en: 'Menu' })}</h2>
+      <p className={st.subtitle}>
+        {t({ ru: 'Приходите к нам у нас весело и вкусно!', en: 'Come to us we have fun and delicious!' })}
+      </p>
       <Collage items={menu.edges} />
     </div>
   )
