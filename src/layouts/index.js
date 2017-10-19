0380154cd51dc6876946e90868ce4a347a08440b
Alexander Kurganow <me@akurganow.ru>
2017-10-19 11:12:02 +0300

    add locales to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
@@ -9,28 +9,43 @@ import { Modals } from '../components/Modals'
 
 import './style.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Modals />
-    <Header />
-    <div>{children()}</div>
+class TemplateWrapper extends Component {
+  state = { locale: 'ru', locales: ['ru', 'en'] }
+  switchLocale = locale => {
+    this.setState({ locale })
+  }
+  t = translates => {
+    return translates[this.state.locale]
+  }
+  render() {
+    return (
+      <div>
+        <Modals t={this.t} />
+        <Header
+          {...this.state}
+          t={this.t}
+          switchLocale={this.switchLocale} />
 
-    <Footer />
-    <Sprite />
-    <Helmet>
-      <title>Cuba Bar</title>
-      <meta
-        name="description"
-        content="Cuba Bar" />
-      <meta
-        name="keywords"
-        content="cuba, bar, gorohovoya" />
-      <link
-        href="https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i|Podkova:400,600&amp;subset=cyrillic,cyrillic-ext,latin-ext"
-        rel="stylesheet" />
-    </Helmet>
-  </div>
-)
+        {this.props.children({ ...this.props, ...this.state, t: this.t })}
+
+        <Footer t={this.t} />
+        <Sprite />
+        <Helmet>
+          <title>Cuba Bar</title>
+          <meta
+            name="description"
+            content="Cuba Bar" />
+          <meta
+            name="keywords"
+            content="cuba, bar, gorohovoya" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i|Podkova:400,600&amp;subset=cyrillic,cyrillic-ext,latin-ext"
+            rel="stylesheet" />
+        </Helmet>
+      </div>
+    )
+  }
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
