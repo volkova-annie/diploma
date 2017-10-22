0c558ad6cf27cd6f6c7c1bbec52a024421fbed9e
Anna Volkova <annie_v@bk.ru>
2017-10-22 22:23:50 +0300

    add preview to the End step of booking modal

--- src/components/Modals/modals/Booking/Steps/End.js
+++ src/components/Modals/modals/Booking/Steps/End.js
@@ -3,7 +3,12 @@ import st from './style.module.css'
 
 const End = (props) =>
   <div className={st.end}>
-    End
+    <p>
+      Дорогой <span>Саша</span>!
+    </p>
+    <p>
+    Вы забронировали столик на <span>6</span> человек(а) на <span>19.11.2017</span> на <span>19:00</span>.
+    </p>
   </div>
 
 export {End}
