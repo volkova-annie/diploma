cc21c83a59267d4bbbdfcb966408529eb9f911f3
Alexander Kurganow <me@akurganow.ru>
2017-11-19 20:23:16 +0300

    Add pages templates

---
+++ src/templates/gallery-single.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import Link from 'gatsby-link'
+import cn from 'classnames'
+import {connectComponent} from '../state/connectComponent'
+import PageLayout from '../components/PageLayout'
+
+class GalleryPost extends Component {
+  render() {
+    const t = this.props.actions.translate
+    const locale = this.props.locale
+    const {data} = this.props.pathContext
+    const item = data[locale]
+
+    return <PageLayout
+      {...this.state}
+      {...this.props}>
+      <div>{item.title}</div>
+    </PageLayout>
+  }
+}
+
+export default connectComponent(GalleryPost)
