206fc0be591a7d97c695a4d53091517634af3066
Anna Volkova <annie_v@bk.ru>
2017-11-22 22:50:08 +0300

    rename reducer type

--- src/reducers/locale.js
+++ src/reducers/locale.js
@@ -1,7 +1,7 @@
 /* eslint-disable require-jsdoc */
 export function locale(state = {}, action) {
   switch (action.type) {
-  case '@@locale/SWITCH_LOCALE': {
+  case 'locale/SWITCH_LOCALE': {
     return action.locale
   }
   default:
