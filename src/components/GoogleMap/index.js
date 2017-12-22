423a023f083bfdf36a4ef63e7868eece4228e259
Anna Volkova <annie_v@bk.ru>
2017-12-22 21:14:39 +0300

    add API key for google maps

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -13,7 +13,7 @@ const MapWithAMarker = withScriptjs(
 
 const GoogleMap = () => (
   <MapWithAMarker
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5AtTPZ10DQXqbmFUrs60GO0JQqTr-5fY&v=3.exp&libraries=geometry,drawing,places"
+    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5lt3UyzYYHny3Gl7dlUeY8YaNmC27pqQ&v=3.exp&libraries=geometry,drawing,places"
     loadingElement={<div style={{ height: '100%' }} />}
     containerElement={<div style={{ height: '400px' }} />}
     mapElement={<div style={{ height: '100%' }} />} />
